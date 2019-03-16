a158f9df49ab6abfb1aadfaac7ff36f64da4745a
wophy <997432625@qq.com>
2019-03-16 09:19:56 +0800

    pinyougou_order 1st commit

--- pinyougou-parent/pinyougou-interface/src/main/java/cn/itcast/core/service/order/OrderService.java
+++ pinyougou-parent/pinyougou-interface/src/main/java/cn/itcast/core/service/order/OrderService.java
@@ -1,10 +1,23 @@
 package cn.itcast.core.service.order;
 
+import cn.itcast.core.pojo.good.Goods;
 import cn.itcast.core.pojo.order.Order;
+import entity.PageResult;
 
 /**
  * @author wophy
  */
 public interface OrderService {
     void add(Order order,String username);
+
+    /**
+     * 订单查询
+     * @param page
+     * @param rows
+     * @param order
+     * @return
+     */
+    public PageResult search(Integer page, Integer rows, Order order);
+
+    Order findOneById(Long orderId);
 }
